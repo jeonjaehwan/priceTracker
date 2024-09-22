2ee1e222b9ea2101c4b7dd75abcf319c0e14ec7e
jeonjaehwan <jeonjaehwan12@naver.com>
2024-09-22 18:40:17 +0900

    consumer

---
+++ price-storage-service/src/main/java/com/pricewatcher/price_storage_service/PriceStorageServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pricewatcher.price_storage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class PriceStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceStorageServiceApplication.class, args);
+	}
+
+}
---
+++ price-storage-service/src/test/java/com/pricewatcher/price_storage_service/PriceStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricewatcher.price_storage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
